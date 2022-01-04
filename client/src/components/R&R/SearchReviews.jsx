44a1d41a9392f88063cbf42cc922020e91b7d0a9
mathew dziedzic <mdzie195@gmail.com>
2022-01-04 01:01:54 -0800

    Made search bar and highlights

---
+++ client/src/components/R&R/SearchReviews.jsx
@@ -0,0 +1,47 @@
+import React, {useContext} from 'react';
+import {AppContext} from '../AppContext.jsx';
+import {ReviewContext} from './ReviewContext.jsx';
+
+export const SearchReviews = () => {
+  const {reviews} = useContext(AppContext);
+  const {searchTerm, setSearchTerm, setSortedBy} = useContext(ReviewContext);
+
+  const sortReviews = (type) => {
+    console.log('CHANGING SORT:', type, reviews.data.results);
+    if (type === 'Most Helpful') {
+      reviews.data.results.sort((review, nextReview) =>
+      (review.helpfulness > nextReview.helpfulness) ? -1 : 1);
+    } else if (type === 'Relevant') {
+      reviews.data.results.sort((review, nextReview) =>
+      (review.helpfulness || nextReview.helpfulness) ? (review.helpfulness/(new Date().getTime() -new Date(review.date).getTime()) > nextReview.helpfulness/(new Date().getTime() - new Date(nextReview.date).getTime())) ? -1 : 1
+      :(new Date(review.date) > new Date(nextReview.date)) ? -1 : 1);
+    } else if (type === 'Newest') {
+      reviews.data.results.sort((review, nextReview) =>
+      (new Date(review.date) > new Date(nextReview.date)) ? -1 : 1);
+    } else if (type === 'Search') {
+      reviews.data.results.sort((review, nextReview) =>
+      (review.body.includes(searchTerm) &&nextReview.body.includes(searchTerm))?
+      (review.helpfulness || nextReview.helpfulness) ? (review.helpfulness/(new Date().getTime() -new Date(review.date).getTime()) > nextReview.helpfulness/(new Date().getTime() - new Date(nextReview.date).getTime())) ? -1 : 1
+      :(new Date(review.date) > new Date(nextReview.date)) ? -1 : 1 :
+      (review.body.includes(searchTerm) >nextReview.body.includes(searchTerm) ) ? -1 : 1);
+    } else {
+      console.log('WHAT KIND OF SORT?', type);
+    }
+    console.log('after--', reviews.data.results);
+    setSortedBy(type);
+  };
+
+
+  return (
+    <span style={{display: 'flex', justifyContent: 'space-between'}}><span>
+      <span>search</span>
+      <input value = {searchTerm} onChange = {(e)=> {setSearchTerm(e.target.value);searchTerm.length>=3?sortReviews('Search'):null}}></input>
+    </span><span>{reviews.data.results.length} reviews, sorted by <select onChange= {(e) => {sortReviews(e.target.value)}}>
+      <option >Relevant</option>
+      <option >Most Helpful</option>
+      <option >Newest</option>
+    </select>
+    </span>
+    </span>
+  );
+};
